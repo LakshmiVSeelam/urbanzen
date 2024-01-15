8deb96937ef3c8f10ef70563192b4305b5b2e18a
LakshmiVSeelam <lakshmivseelam@gmail.com>
2024-01-15 17:49:08 +0530

    standalone routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,21 +1,54 @@
 import { Routes } from '@angular/router';
-import { HomeComponent } from './home/home.component';
-import { PortfolioComponent } from './portfolio/portfolio.component';
-import { ExperienceComponent } from './experience/experience.component';
-import { MediaComponent } from './media/media.component';
-import { AboutComponent } from './about/about.component';
 import { LandComponent } from './experience/land/land.component';
 import { ArchitectureComponent } from './experience/architecture/architecture.component';
 import { InteriorsComponent } from './experience/interiors/interiors.component';
 
 export const routes: Routes = [
-    { path: '', component: HomeComponent, title: 'Home' },
-    { path: 'portfolio', component: PortfolioComponent , title: 'Portfolio'},
-    { path: 'experience', component: ExperienceComponent, title: 'Experience', children: [
-        {path: 'land', component: LandComponent},
-        {path: 'architecture', component: ArchitectureComponent},
-        {path: 'interiors', component: InteriorsComponent}
-    ] },
-    { path: 'media', component: MediaComponent, title: 'Media & Awards' },
-    { path: 'about', component: AboutComponent, title: 'About Us' },
+  {
+    path: '',
+    title: 'Home',
+    loadComponent: () =>
+      import('./home/home.component').then((m) => m.HomeComponent),
+  },
+  {
+    path: 'portfolio',
+    title: 'Portfolio',
+    loadComponent: () =>
+      import('./portfolio/portfolio.component').then(
+        (m) => m.PortfolioComponent
+      ),
+  },
+  {
+    path: 'experience',
+    title: 'Experience',
+    loadComponent: () =>
+      import('./experience/experience.component').then(
+        (m) => m.ExperienceComponent
+      ),
+    children: [
+      { path: 'land', component: LandComponent },
+      { path: 'architecture', component: ArchitectureComponent },
+      { path: 'interiors', component: InteriorsComponent },
+    ],
+  },
+  {
+    path: 'media',
+    title: 'Media & Awards',
+    loadComponent: () =>
+      import('./media/media.component').then((m) => m.MediaComponent),
+  },
+  {
+    path: 'about',
+    title: 'About',
+    loadComponent: () =>
+      import('./about/about.component').then((m) => m.AboutComponent),
+  },
+  // { path: 'portfolio', component: PortfolioComponent , title: 'Portfolio'},
+  // { path: 'experience', component: ExperienceComponent, title: 'Experience', children: [
+  //     {path: 'land', component: LandComponent},
+  //     {path: 'architecture', component: ArchitectureComponent},
+  //     {path: 'interiors', component: InteriorsComponent}
+  // ] },
+  // { path: 'media', component: MediaComponent, title: 'Media & Awards' },
+  // { path: 'about', component: AboutComponent, title: 'About Us' },
 ];
